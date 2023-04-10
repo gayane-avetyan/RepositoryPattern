d27db9fadd7e2038d33cad2a19bb48dc4cbf0244
gayane-avetyan <avetyan.gayane.01@gmail.com>
2023-04-10 22:31:28 +0400

    add actions for controller

--- GenericRepository/Controllers/WeatherForecastController.cs
+++ GenericRepository/Controllers/WeatherForecastController.cs
@@ -15,10 +15,91 @@ namespace GenericRepository.Controllers
             _repository = repository;
         }
 
-        [HttpPost]
-        public IActionResult Set([FromBody] WeatherForecast request)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var entity = await _repository.Get(id);
+
+            return Ok(entity);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetBySummary(string summary)
+        {
+            var entity = await _repository.Find(m => m.Summary == summary);
+
+            return Ok(entity);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAll()
+        {
+            var entity = await _repository.GetAll();
+
+            return Ok(entity);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllBySummary(string summary)
+        {
+            var entity = await _repository.GetWhere(m => m.Summary == summary);
+
+            return Ok(entity);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Save([FromBody] WeatherForecast request)
         {
             _repository.Add(request);
+            _repository.Save();
+
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult SaveAll([FromBody] WeatherForecast[] request)
+        {
+            _repository.AddRange(request);
+            _repository.Save();
+
+            return Ok();
+        }
+
+        [HttpPut("[action]")]
+        public IActionResult Update([FromBody] WeatherForecast request)
+        {
+            _repository.Update(request);
+            _repository.Save();
+
+            return Ok();
+        }
+
+        [HttpPut("[action]")]
+        public IActionResult UpdateAll([FromBody] WeatherForecast[] request)
+        {
+            _repository.UpdateRange(request);
+            _repository.Save();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var entity = _repository.Find(e => e.Id == id).Result;
+
+            _repository.Delete(entity);
+            _repository.Save();
+
+            return Ok();
+        }
+
+        [HttpDelete("[action]")]
+        public IActionResult RemoveAll(string summary)
+        {
+            var entity = _repository.GetWhere(m => m.Summary == summary).Result;
+
+            _repository.DeleteRange(entity);
             _repository.Save();
 
             return Ok();
